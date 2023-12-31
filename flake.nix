cb32ea70f3f9590ee0b5de44f419ea97ba55614a
Greg Pfeil <greg@technomadic.org>
2023-12-31 09:43:28 +0100

    Update Home Manager to release-23.11

--- flake.nix
+++ flake.nix
@@ -276,7 +276,7 @@
 
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
     };
 
     ## Avoids the need to give `Finder` access to make aliases on MacOS.
