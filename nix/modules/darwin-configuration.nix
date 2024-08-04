6789c76732078dc009e1045b4798204951818fa9
Greg Pfeil <greg@technomadic.org>
2024-08-04 00:49:51 -0600

    Update font config for darwin

--- nix/modules/darwin-configuration.nix
+++ nix/modules/darwin-configuration.nix
@@ -36,13 +36,10 @@
     ];
   };
 
-  fonts = {
-    fontDir.enable = false; # enabling this trashes manually-installed fonts
-    fonts = [
-      pkgs.atkinson-hyperlegible # https://brailleinstitute.org/freefont
-      pkgs.inconsolata
-    ];
-  };
+  fonts.packages = [
+    pkgs.atkinson-hyperlegible # https://brailleinstitute.org/freefont
+    pkgs.inconsolata
+  ];
 
   # The preferred location of applications is, in order:
   # 1. home.nix#home.packages (~/Applications/Home Manager Apps)
