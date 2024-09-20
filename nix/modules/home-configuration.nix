973218e48816d43995ae21b5b080462224698a38
Greg Pfeil <greg@technomadic.org>
2024-09-20 12:29:30 -0600

    Make it easier to define systems downstream
    Adds functions that set up all the `specialArgs`, which means downstream
    configurations only need to include ones that they explicitly depend on and
    don’t need to duplicate so many flake inputs.
    
    This also removes `stateVersion` from the configurations, as that should be set
    by the final configuration downstream, and only bumped once that configuration
    is confident it has addressed any possible issues.

--- nix/modules/home-configuration.nix
+++ nix/modules/home-configuration.nix
@@ -1,4 +1,5 @@
 {
+  agenix,
   config,
   dotfiles,
   lib,
@@ -7,6 +8,7 @@
   ...
 }: {
   imports = [
+    agenix.homeManagerModules.age
     ./emacs
     ./i3.nix
     ./input-devices.nix
@@ -410,16 +412,6 @@
       ## Include dotfiles.
       tree = "tree -a";
     };
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "23.05";
   };
 
   lib.local = {
