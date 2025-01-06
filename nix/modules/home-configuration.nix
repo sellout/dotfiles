f20aafba4781b7b558cd2570e267e3ae0ff5f7ac
Greg Pfeil <greg@technomadic.org>
2025-01-06 13:11:14 -0700

    Add variables to match the Solarized theme

--- nix/modules/home-configuration.nix
+++ nix/modules/home-configuration.nix
@@ -401,6 +401,65 @@
     addHome = path: config.home.homeDirectory + "/" + path;
     removeHome = lib.removePrefix (config.home.homeDirectory + "/");
 
+    ## These make it easy to match the Solarized theme
+    ## (https://ethanschoonover.com/solarized/) for modules that support color
+    ## configuration.
+    solarized = mode: let
+      darkColors = {
+        base03 = "#002b36";
+        base02 = "#073642";
+        base01 = "#586e75";
+        base00 = "#657b83";
+        base0 = "#839496";
+        base1 = "#93a1a1";
+        base2 = "#eee8d5";
+        base3 = "#fdf6e3";
+        blue = "#268bd2";
+        cyan = "#2aa198";
+        green = "#859900";
+        magenta = "#d33682";
+        orange = "#cb4b16";
+        red = "#dc322f";
+        violet = "#6c71c4";
+        yellow = "#b58900";
+      };
+      color =
+        if mode == "dark"
+        then darkColors
+        else
+          darkColors
+          // {
+            base03 = darkColors.base3;
+            base02 = darkColors.base2;
+            base01 = darkColors.base1;
+            base00 = darkColors.base0;
+            base0 = darkColors.base00;
+            base1 = darkColors.base01;
+            base2 = darkColors.base02;
+            base3 = darkColors.base03;
+          };
+    in {
+      inherit color;
+      background = color.base03;
+      ANSI = {
+        normal = {
+          inherit (color) blue cyan green magenta red yellow;
+          black = color.base02;
+          white = color.base2;
+        };
+        bright = {
+          black = color.base03;
+          blue = color.base0;
+          cyan = color.base1;
+          green = color.base01;
+          magenta = color.violet;
+          red = color.orange;
+          white = color.base3;
+          yellow = color.base00;
+        };
+      };
+    };
+
     # Variables that `config.xdg` doesn’t provide, but that I wish it would.
     xdg = {
       bin = {
