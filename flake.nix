b90d4667a1d882d0fc1f7aab1bf74c4d4c4df259
Greg Pfeil <greg@technomadic.org>
2025-08-08 12:49:23 -0600

    Add my personal Cachix cache
    Some things are just too long-running to expect garnix to build, so build
    locally and push to Cachix, to take the pressure off garnix.

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   nixConfig = {
     ## https://github.com/NixOS/rfcs/blob/master/rfcs/0045-deprecate-url-syntax.md
     extra-experimental-features = ["no-url-literals"];
-    extra-substituters = ["https://cache.garnix.io"];
+    extra-substituters = [
+      "https://cache.garnix.io"
+      "https://sellout.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "sellout.cachix.org-1:v37cTpWBEycnYxSPAgSQ57Wiqd3wjljni2aC0Xry1DE="
     ];
     ## Isolate the build.
     use-registries = false;
