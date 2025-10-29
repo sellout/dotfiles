017f459adbbd52db9dc45fb436c740ee0c6037dc
Greg Pfeil <greg@technomadic.org>
2025-10-29 12:56:14 -0600

    Silence Nix profile upgrades

--- nix/modules/darwin/default.nix
+++ nix/modules/darwin/default.nix
@@ -154,7 +154,7 @@
     ## NB: For this to work, I had to initially replace the original packages in
     ##     the profile with ones from the nixpkgs flake.
     echo "upgrading default nix profile ..."
-    sudo nix profile upgrade --all --profile /nix/var/nix/profiles/default >/dev/null
+    sudo nix profile upgrade --all --profile /nix/var/nix/profiles/default &>/dev/null
 
     ## List manual tasks that happen after activating a new configuration.
     echo "Now that a new configuration is activated, there are some steps that need to be"
