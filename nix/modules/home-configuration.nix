94d8840455985dc758252e78acb35077f8c076ac
Greg Pfeil <greg@technomadic.org>
2026-09-14 15:12:41 -0600

    Update SHA for PowerPhotos

--- nix/modules/home-configuration.nix
+++ nix/modules/home-configuration.nix
@@ -160,7 +160,7 @@
         (pkgs.brewCasks.powerphotos.overrideAttrs (old: {
           src = pkgs.fetchurl {
             url = builtins.head old.src.urls;
-            hash = "sha256-zjpYk9/LHbF8RzpWne7d2u9j5qqnZ1bhoHCcOUE1MU8=";
+            hash = "sha256-AXudYvVX5wteGruSdW/2OgvWFqEesHhkFmtJlJ7VSIU=";
           };
         }))
         pkgs.brewCasks.processing
